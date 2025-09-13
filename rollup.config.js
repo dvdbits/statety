3bc9d875dea729848b7c2fc8f9cb1e31123b4522
Cioara David-Darius <62652359+dvdbits@users.noreply.github.com>
2025-09-13 13:43:08 +0300

    Initial project setup

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    { file: 'dist/index.cjs.js', format: 'cjs', sourcemap: true },
+    { file: 'dist/index.esm.js', format: 'esm', sourcemap: true },
+  ],
+  external: ['react', 'react-dom'],
+  plugins: [typescript({ useTsconfigDeclarationDir: true })],
+};
